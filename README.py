ce92402929d5aca60460fd1c955396a635beb5b6
yvesyuzo <yvesyuzo@gmail.com>
2015-05-12 10:28:43 -0300

    Update README.py

--- README.py
+++ README.py
@@ -3,11 +3,14 @@ Projeto final de SoftDes do primeiro semestre.
 ***
 O Guitar-Teacher vai ensinar o usuario a tocar guitarra ou violão.
 
-Para isso uma webcam deve estar filmando o braço do instrumento e na tela 
-sera demonstrado a posição correta dos dedos em tempo real.
+--EDIT--
+Para isso o programa vai monstrar na tela a sequencia das notas que devem ser tocadas, e receber pelo MIC os sons do violão
+comparando-os com um banco de dados interno para saber se o usuario esta tocando a nota certa.
+#Para isso uma webcam deve estar filmando o braço do instrumento e na tela 
+#sera demonstrado a posição correta dos dedos em tempo real.
 
-Para verificar se as notas estão corretas o programa ira reconhecer as 
-frequencias sonoras reproduzidas. Assim, o programa podera alem de dizer 
-se as notas estão certas, monstrar como elas devem ser tocadas.
+#Para verificar se as notas estão corretas o programa ira reconhecer as 
+#frequencias sonoras reproduzidas. Assim, o programa podera alem de dizer 
+#se as notas estão certas, monstrar como elas devem ser tocadas.
 
 ***
