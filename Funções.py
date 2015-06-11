24c645f9df0614df5f087c96ff68f8ead2f1d12f
yvesyuzo <yvesyuzo@gmail.com>
2015-06-11 17:23:54 -0300

    Analiza as Funções
    Debug da função analiza_som()

--- Funções.py
+++ Funções.py
@@ -13,6 +13,12 @@ import time
 import matplotlib.pyplot as plt
 
 
+CHUNK = 2048
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+p = pyaudio.PyAudio()
+
 def grava_som(NOME, segundos):
 
     CHUNK = 2048
@@ -41,11 +47,9 @@ def grava_som(NOME, segundos):
     frames = []
     
     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        print('data:',len(data))
-        print('type data ',type(data))
-        frames.append(data)
-    
+        data = stream.read(CHUNK) #data é do tipo 'bytes' e tem o len = 4096 
+
+        frames.append(data)    
     print('frames:',len(frames))
     print("* done recording")
     
@@ -64,7 +68,131 @@ def grava_som(NOME, segundos):
     wf.close()
 
 
-grava_som('jovem2', 3)
 
+
+def analiza_som():
+    
+    CHUNK = 2048
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    
+    
+    
+    # open up a wave
+    wf = wave.open('jovem2.wav', 'rb')
+    swidth = wf.getsampwidth()
+    print('swidth',swidth)
+    RATE = wf.getframerate()
+    
+    # use a Blackman window
+    window = np.blackman(CHUNK)
+    
+    # open stream
+    p = pyaudio.PyAudio()
+    stream = p.open(format =
+                    p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = RATE,
+                    output = True)
+    
+    
+    
+    maior_data = 0
+    
+    
+    
+    # read some data
+    data = wf.readframes (int(CHUNK)) 
+    # play stream and find the frequency of each chunk
+    print(len(data), CHUNK*swidth)
+    freqs=[]
+    
+    while len(data) == int(CHUNK)*swidth:
+        
+        # write data out to the audio stream
+        stream.write(data)
+        # unpack the data and times by the hamming window
+        #try:
+        indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth),data))*window
+        print("Indata ok")
+        
+        # Take the fft and square each value
+        fftData=abs(np.fft.rfft(indata))**2
+        # find the maximum
+        which = fftData[1:].argmax() + 1
+    
+        print("fft data: ", fftData[which])
+        
+        
+         
+        if fftData[which] > maior_data:
+            maior_data = fftData[which]
+        
+    #    if fftData[which]<threshold:
+            
+            #desconsidere frequencias
+        
+        
+        # use quadratic interpolation around the max
+        if which != len(fftData)-1:
+            y0,y1,y2 = np.log(fftData[which-1:which+2:])
+            x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+            # find the frequency and output it
+            thefreq = (which+x1)*RATE/CHUNK
+            print ("The freq is %f Hz." % (thefreq))
+            freqs.append(thefreq)
+        else:
+            thefreq = which*RATE/CHUNK
+            print ("The freq is %f Hz." % (thefreq))
+            freqs.append(thefreq)
+        # read some more data
+        data = wf.readframes (int(CHUNK))
+        print ("------chunk ok ------")
+        
+#        testd.append(fftData[1])    
+    #    except:
+    #        indata = None        
+    #        print ("Not enough data")
+    #    plt.plot((fftData))
+    #    plt.ylabel("dividido por 2")
+    #    plt.show()
+    #    plt.close()
+    print('done')
+    
+    print('As freqs são:')
+    for i in freqs:
+        print(i)
+    
+    print("O len das freqs é: ", len(freqs))
+    print (type(freqs))
+    #print('O tipo de cada freq é: ', type(freqs[2]))
+    
+    #plt.plot(freqs[int(len(freqs)*0.1):int(len(freqs)*0.9)])
+    plt.plot(freqs)
+    plt.grid()
+    plt.show()
+ 
+
+
+#grava_som('jovem2', 3)       
+#    
+analiza_som()
+
+
+
+#wf = wave.open('jovem2', 'wb')
+#wf.setnchannels(CHANNELS)
+#wf.setsampwidth(p.get_sample_size(FORMAT))
+#wf.setframerate(RATE)
+#wf.writeframes(b''.join(frames))
+#wf.close()
+#    
+    
+    
+    
+    
+    
+    
 
 
