770467bf5571fc1938948f5c9ab245a14a1c3c7a
yvesyuzo <yvesyuzo@gmail.com>
2020-12-22 19:02:11 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,19 +1,21 @@
-# Guitar-Teacher
-Projeto final de SoftDes do primeiro semestre.
-***
-O Guitar-Teacher vai ensinar o usuario a tocar guitarra ou violão.
+### Date created
+24/06/2015
+dd/mm/yyyy
 
---EDIT--
-Para isso o programa vai monstrar na tela a sequencia das notas que devem ser tocadas, e receber pelo MIC os sons do violão
-comparando-os com um banco de dados interno para saber se o usuario esta tocando a nota certa.
+### Project Title
+Guitar Teacher
 
+### Description
+Final project from Software Desgin subject at Insper Engineering course
+  - This program is made to teach a user to play guitar
+  - The original intent was to show the notes in a similar fashion than Guitar Hero and by recording the sound 
+    be able to tell if it was the correct note.
+  - But we were only able to finish the sound recognition so it works more like a guitar tuner.
+  - The program has a graphical interface that requires mic access to record and analise frequencies played
+  - Similar to guitar tuner apps it shows which note you have played
 
+### Files used
+Guitar on Pygame.py
+funcs.py
 
-#Para isso uma webcam deve estar filmando o braço do instrumento e na tela 
-#sera demonstrado a posição correta dos dedos em tempo real.
-
-#Para verificar se as notas estão corretas o programa ira reconhecer as 
-#frequencias sonoras reproduzidas. Assim, o programa podera alem de dizer 
-#se as notas estão certas, monstrar como elas devem ser tocadas.
-
-***
+### Credits
